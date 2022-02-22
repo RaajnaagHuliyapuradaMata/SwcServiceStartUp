db85e24c143a1ce2b389c99c7155a85158b9c034
root <nagaraja.hm.0001@gmail.com>
2022-02-22 12:12:29 +0100

    Update: submodule BswAUTOSAR_Service_StartUp

--- LATEST/StartUp.cpp
+++ LATEST/StartUp.cpp
@@ -1,7 +1,7 @@
 //#include "AUTOSAR_Cfg.h"
-
-#include "Std_Types.h"
-#include "Compiler_Cfg_StartUp.h"
+#include "module.h"
+#include "StartUp_EcuM.h"
+#include "StartUp_SchM.h"
 
 #include "EcuM_StartUp.h"
 
@@ -13,6 +13,32 @@ typedef enum{
 }t_Shutdown;
 
 t_Shutdown e_Shutdown = E_SHUTDOWN_NORMAL;
+
+class module_StartUp:
+      public abstract_module
+   ,  public interface_StartUp_EcuM
+   ,  public interface_StartUp_SchM
+{
+   public:
+      FUNC(void, BSWM_CODE) InitFunction   (void);
+      FUNC(void, BSWM_CODE) DeInitFunction (void);
+      FUNC(void, BSWM_CODE) MainFunction   (void);
+};
+
+module_StartUp StartUp;
+
+interface_StartUp_EcuM *EcuM_Client_ptr_StartUp = &StartUp;
+interface_StartUp_SchM *SchM_Client_ptr_StartUp = &StartUp;
+
+FUNC(void, BSWM_CODE) InitFunction(void){
+}
+
+FUNC(void, BSWM_CODE) DeInitFunction(void){
+}
+
+FUNC(void, BSWM_CODE) MainFunction(void){
+}
+
 
 int main(
 #if(_ReSIM == STD_ON)
