3501fcd0796619d69d5e93c207a25921b63153b6
root <nagaraja.hm.0001@gmail.com>
2022-03-21 13:08:02 +0100

    Update: AUTOSAR basic software service module StartUp

--- LATEST/StartUp.cpp
+++ LATEST/StartUp.cpp
@@ -6,9 +6,8 @@
 /******************************************************************************/
 /* #INCLUDES                                                                  */
 /******************************************************************************/
-#include "infStartUp_Version.h"
-
 #include "module.h"
+#include "infStartUp_Version.h"
 #include "infStartUp_EcuM.h"
 #include "infStartUp_Dcm.h"
 #include "infStartUp_SchM.h"
@@ -46,31 +45,20 @@ class module_StartUp:
 /******************************************************************************/
 /* OBJECTS                                                                    */
 /******************************************************************************/
-
-/******************************************************************************/
-/* FUNCTIONS                                                                  */
-/******************************************************************************/
-
-/******************************************************************************/
-/* EOF                                                                        */
-/******************************************************************************/
-
-
-/*****************************************************/
-/* OBJECTS                                           */
-/*****************************************************/
 VAR(module_StartUp, STARTUP_VAR) StartUp;
 CONSTP2VAR(infEcuMClient, STARTUP_VAR, STARTUP_CONST) gptrinfEcuMClient_StartUp = &StartUp;
 CONSTP2VAR(infDcmClient,  STARTUP_VAR, STARTUP_CONST) gptrinfDcmClient_StartUp  = &StartUp;
 CONSTP2VAR(infSchMClient, STARTUP_VAR, STARTUP_CONST) gptrinfSchMClient_StartUp = &StartUp;
 
-/*****************************************************/
-/* FUNCTIONS                                         */
-/*****************************************************/
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
 FUNC(void, STARTUP_CODE) module_StartUp::InitFunction(void){
+   StartUp.IsInitDone = E_OK;
 }
 
 FUNC(void, STARTUP_CODE) module_StartUp::DeInitFunction(void){
+   StartUp.IsInitDone = E_NOT_OK;
 }
 
 FUNC(void, STARTUP_CODE) module_StartUp::GetVersionInfo(void){
@@ -104,7 +92,7 @@ int main(
    return e_Shutdown;
 }
 
-/*****************************************************/
-/* EOF                                               */
-/*****************************************************/
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
 
