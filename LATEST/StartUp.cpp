0c9da3b31f2ad77a445b6b7e1b4dcb98d7b15d42
root <nagaraja.hm.0001@gmail.com>
2022-03-23 13:09:21 +0100

    Update: AUTOSAR basic software service module StartUp

--- LATEST/StartUp.cpp
+++ LATEST/StartUp.cpp
@@ -41,6 +41,16 @@ class module_StartUp:
       FUNC(void, STARTUP_CODE) DeInitFunction (void);
       FUNC(void, STARTUP_CODE) GetVersionInfo (void);
       FUNC(void, STARTUP_CODE) MainFunction   (void);
+
+   private:
+      CONST(Std_TypeVersionInfo, STARTUP_CONST) VersionInfo = {
+            0x0000
+         ,  0xFFFF
+         ,  0x01
+         ,  '0'
+         ,  '1'
+         ,  '0'
+      };
 };
 
 /******************************************************************************/
