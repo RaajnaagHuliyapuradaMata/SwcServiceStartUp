6f2398cbe989bae33cf8525e113b27ec80b8ca52
root <nagaraja.hm.0001@gmail.com>
2022-03-29 13:49:09 +0200

    Update: AUTOSAR basic software service module StartUp

--- LATEST/StartUp.cpp
+++ LATEST/StartUp.cpp
@@ -7,7 +7,6 @@
 /* #INCLUDES                                                                  */
 /******************************************************************************/
 #include "module.hpp"
-//#include "CfgStartUp.hpp"
 #include "infStartUp_EcuM.hpp"
 #include "infStartUp_Dcm.hpp"
 #include "infStartUp_SchM.hpp"
@@ -15,7 +14,6 @@
 /******************************************************************************/
 /* #DEFINES                                                                   */
 /******************************************************************************/
-#define _ReSIM                                                                 0
 #define STARTUP_AR_RELEASE_MAJOR_VERSION                                       4
 #define STARTUP_AR_RELEASE_MINOR_VERSION                                       3
 
@@ -37,37 +35,40 @@ class module_StartUp:
       public abstract_module
 {
    public:
+      module_StartUp(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
+      }
       FUNC(void, STARTUP_CODE) InitFunction   (void);
       FUNC(void, STARTUP_CODE) DeInitFunction (void);
-      FUNC(void, STARTUP_CODE) GetVersionInfo (void);
       FUNC(void, STARTUP_CODE) MainFunction   (void);
-
-   private:
-      CONST(Std_TypeVersionInfo, STARTUP_CONST) VersionInfo = {
-            0x0000
-         ,  0xFFFF
-         ,  0x01
-         ,  '0'
-         ,  '1'
-         ,  '0'
-      };
 };
+
+extern VAR(module_StartUp, STARTUP_VAR) StartUp;
 
 /******************************************************************************/
 /* CONSTS                                                                     */
 /******************************************************************************/
+CONSTP2VAR(infEcuMClient, STARTUP_VAR, STARTUP_CONST) gptrinfEcuMClient_StartUp = &StartUp;
+CONSTP2VAR(infDcmClient,  STARTUP_VAR, STARTUP_CONST) gptrinfDcmClient_StartUp  = &StartUp;
+CONSTP2VAR(infSchMClient, STARTUP_VAR, STARTUP_CONST) gptrinfSchMClient_StartUp = &StartUp;
 
 /******************************************************************************/
 /* PARAMS                                                                     */
 /******************************************************************************/
+//#include "CfgStartUp.hpp"
 
 /******************************************************************************/
 /* OBJECTS                                                                    */
 /******************************************************************************/
-VAR(module_StartUp, STARTUP_VAR) StartUp;
-CONSTP2VAR(infEcuMClient, STARTUP_VAR, STARTUP_CONST) gptrinfEcuMClient_StartUp = &StartUp;
-CONSTP2VAR(infDcmClient,  STARTUP_VAR, STARTUP_CONST) gptrinfDcmClient_StartUp  = &StartUp;
-CONSTP2VAR(infSchMClient, STARTUP_VAR, STARTUP_CONST) gptrinfSchMClient_StartUp = &StartUp;
+VAR(module_StartUp, STARTUP_VAR) StartUp(
+   {
+         0x0000
+      ,  0xFFFF
+      ,  0x01
+      ,  '0'
+      ,  '1'
+      ,  '0'
+   }
+);
 
 /******************************************************************************/
 /* FUNCTIONS                                                                  */
@@ -78,14 +79,6 @@ FUNC(void, STARTUP_CODE) module_StartUp::InitFunction(void){
 
 FUNC(void, STARTUP_CODE) module_StartUp::DeInitFunction(void){
    StartUp.IsInitDone = E_NOT_OK;
-}
-
-FUNC(void, STARTUP_CODE) module_StartUp::GetVersionInfo(void){
-#if(STD_ON == StartUp_DevErrorDetect)
-//TBD: API parameter check
-   Det_ReportError(
-   );
-#endif
 }
 
 FUNC(void, STARTUP_CODE) module_StartUp::MainFunction(void){
@@ -99,6 +92,8 @@ typedef enum{
 t_Shutdown e_Shutdown = E_SHUTDOWN_NORMAL;
 
 #include "infEcuM_StartUp.hpp"
+
+#define _ReSIM                                                                 0
 
 int main(
 #if(_ReSIM == STD_ON)
