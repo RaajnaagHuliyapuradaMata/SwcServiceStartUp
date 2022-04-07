d07b9daa3573af9d6bf4745b80ae78e71ade8da9
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-04-07 09:26:14 +0000

    Update: AUTOSAR basic software service module StartUp

--- LATEST/StartUp.cpp
+++ LATEST/StartUp.cpp
@@ -101,8 +101,8 @@ FUNC(void, STARTUP_CODE) module_StartUp::InitFunction(
             lptrCfg = lptrCfgModule;
          }
          else{
-// use PBcfg_CanIf as back-up configuration
-            lptrCfg = PBcfg_CanIf;
+// use PBcfgCanIf as back-up configuration
+//            lptrCfg = &PBcfgStartUp;
          }
       }
       IsInitDone = E_OK;
