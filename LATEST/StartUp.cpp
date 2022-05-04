c9df5a7f42fdcf3af356e3d83a8d27be1fc38ec2
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-04 09:02:16 +0000

    Update: AUTOSAR basic software service module StartUp

--- LATEST/StartUp.cpp
+++ LATEST/StartUp.cpp
@@ -113,7 +113,7 @@ FUNC(void, STARTUP_CODE) module_StartUp::InitFunction(
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  STARTUP_E_UNINIT
       );
 #endif
    }
@@ -133,7 +133,7 @@ FUNC(void, STARTUP_CODE) module_StartUp::DeInitFunction(void){
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  STARTUP_E_UNINIT
       );
 #endif
    }
@@ -152,7 +152,7 @@ FUNC(void, STARTUP_CODE) module_StartUp::MainFunction(void){
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  STARTUP_E_UNINIT
       );
 #endif
    }
