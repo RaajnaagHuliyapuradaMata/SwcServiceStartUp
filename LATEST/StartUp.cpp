a41b95bba568ec708f23e5af5f9d445a57933c46
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-06-29 09:04:14 +0000

    Update: AUTOSAR basic software service module StartUp

--- LATEST/StartUp.cpp
+++ LATEST/StartUp.cpp
@@ -41,12 +41,8 @@ class module_StartUp:
 /******************************************************************************/
 /* OBJECTS                                                                    */
 /******************************************************************************/
-            Std_TypeReturn          IsInitDone{E_NOT_OK};
-      const CfgModule_TypeAbstract* lptrCfg{(CfgModule_TypeAbstract*)NULL_PTR};
 
    public:
-      module_StartUp(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
-      }
       FUNC(void, STARTUP_CODE) InitFunction(
          CONSTP2CONST(CfgModule_TypeAbstract, STARTUP_CONFIG_DATA, STARTUP_APPL_CONST) lptrCfgModule
       );
@@ -71,18 +67,7 @@ CONSTP2VAR(infSchMClient, STARTUP_VAR, STARTUP_CONST) gptrinfSchMClient_StartUp 
 /******************************************************************************/
 /* OBJECTS                                                                    */
 /******************************************************************************/
-VAR(module_StartUp, STARTUP_VAR) StartUp(
-   {
-         STARTUP_AR_RELEASE_VERSION_MAJOR
-      ,  STARTUP_AR_RELEASE_VERSION_MINOR
-      ,  0x00
-      ,  0xFF
-      ,  0x01
-      ,  '0'
-      ,  '1'
-      ,  '0'
-   }
-);
+VAR(module_StartUp, STARTUP_VAR) StartUp;
 
 /******************************************************************************/
 /* FUNCTIONS                                                                  */
