d918fb60dec9115dd2d3376fc7d1e46ac41b6851
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-01-24 21:49:53 +0100

    Update: AUTOSAR basic software service module StartUp

---
+++ main_R2023/infServiceStartUp_ServiceNvM.hpp
@@ -0,0 +1,19 @@
+#pragma once
+/******************************************************************************/
+/* File   : infServiceStartUp_ServiceNvM.hpp                                  */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CONSTSERVICESTARTUP_AR_RELEASE_VERSION_MAJOR                           4
+#define CONSTSERVICESTARTUP_AR_RELEASE_VERSION_MINOR                           3
+
+#define Const_DefaultInit_ConstServiceStartUp                                                                                  \
+   ,  {CONSTSERVICESTARTUP_AR_RELEASE_VERSION_MAJOR, CONSTSERVICESTARTUP_AR_RELEASE_VERSION_MINOR, 0x00,0xFF,0x01,'0','1','0'} \
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
